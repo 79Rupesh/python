9f6fcad34ee618df8e7ac3227154cb3a3ce42a7b
Rupeshkumarbijewar <rupeshkumarbijewar@gmail.com>
2026-03-11 18:16:11 +0530

    tfhg

---
+++ acs.py
@@ -0,0 +1,9 @@
+months = [
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+]
+
+for m in months:
+    print(m)
